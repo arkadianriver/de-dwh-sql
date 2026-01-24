5ba55e163cc1811ffa5a1a5e6a1fe953c1ca4ebd
Gary Faircloth <arkadianriver@gmail.com>
2026-01-24 13:27:50 -0600

    ttl time feature for #12

--- scripts/run_bronze.sh
+++ scripts/run_bronze.sh
@@ -17,10 +17,11 @@ case $1 in
                 ;;
     "load")     sudo -u postgres psql -f bronze/load_bronze.sql 2>&1 | tee "${logroot}-${tstamp}-3-load.log"
                 ;;
-    "all")      sudo -u postgres psql -f init_database.sql  2>&1 | tee "${logroot}-${tstamp}-1-init.log" &&
-                sudo -u postgres psql -f bronze/ddl_bronze.sql 2>&1 | tee "${logroot}-${tstamp}-2-create.log" &&
-                sudo -u postgres psql -f bronze/load_bronze.sql 2>&1 | tee "${logroot}-${tstamp}-3-load.log"
+    "all")      SECONDS=0 &&
+                sudo -u postgres psql -f init_database.sql  2>&1 | tee "${logroot}-${tstamp}-0-all.log" &&
+                sudo -u postgres psql -f bronze/ddl_bronze.sql 2>&1 | tee "${logroot}-${tstamp}-0-all.log" &&
+                sudo -u postgres psql -f bronze/load_bronze.sql 2>&1 | tee "${logroot}-${tstamp}-0-all.log" &&
+                echo -ne "\n=================================\nElapsed time: $SECONDS seconds\n=================================\n" | tee -a "${logroot}-${tstamp}-0-all.log"
                 ;;
     *)          echo "Usage: $0 {init|create|load|all}"
 esac
-
