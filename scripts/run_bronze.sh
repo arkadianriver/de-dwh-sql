77a6cb4ea8bd91104bd4ece49323de0bfa0763b5
Gary Faircloth <arkadianriver@gmail.com>
2026-01-24 12:37:57 -0600

    local log files to assist with #12

--- scripts/run_bronze.sh
+++ scripts/run_bronze.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
+relsdir=$(realpath "${BASH_SOURCE[0]}")
+abssdir=$(dirname "$relsdir")
+absprjdir=$(dirname "$abssdir")
+
+tstamp=$(date +%Y-%m-%d_%H%M%S)
+
+logroot="${absprjdir}/logs/run_bronze"
+
+mkdir -p "${absprjdir}/logs"
+
 case $1 in
-    "init")     sudo -u postgres psql -f init_database.sql ;;
-    "create")   sudo -u postgres psql -f bronze/ddl_bronze.sql ;;
-    "load")     sudo -u postgres psql -f bronze/load_bronze.sql ;;
-    "all")      sudo -u postgres psql -f init_database.sql && sudo -u postgres psql -f bronze/ddl_bronze.sql && sudo -u postgres psql -f bronze/load_bronze.sql ;;
-    *)          echo "Usage: $0 {init|create|load}"
+    "init")     sudo -u postgres psql -f init_database.sql 2>&1 | tee "${logroot}-${tstamp}-1-init.log"
+                ;;
+    "create")   sudo -u postgres psql -f bronze/ddl_bronze.sql 2>&1 | tee "${logroot}-${tstamp}-2-create.log"
+                ;;
+    "load")     sudo -u postgres psql -f bronze/load_bronze.sql 2>&1 | tee "${logroot}-${tstamp}-3-load.log"
+                ;;
+    "all")      sudo -u postgres psql -f init_database.sql  2>&1 | tee "${logroot}-${tstamp}-1-init.log" &&
+                sudo -u postgres psql -f bronze/ddl_bronze.sql 2>&1 | tee "${logroot}-${tstamp}-2-create.log" &&
+                sudo -u postgres psql -f bronze/load_bronze.sql 2>&1 | tee "${logroot}-${tstamp}-3-load.log"
+                ;;
+    *)          echo "Usage: $0 {init|create|load|all}"
 esac
+
