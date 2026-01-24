bdab0c412ec8a75cbf0bf3d151798cde58197cb3
Gary Faircloth <arkadianriver@gmail.com>
2026-01-24 16:27:36 -0600

    data flow diagram started. fixes #11

--- docs/build.sh
+++ docs/build.sh
@@ -6,13 +6,17 @@ case $1 in
 				;;
 	"html")     mkdir -p out/html
 				asciidoctor src/index.adoc -o out/html/index.html
+				cp src/*.png out/html/
+				cp src/*.svg out/html/
 				;;
 	"pdf")      mkdir -p out/pdf
 				asciidoctor-pdf src/index.adoc -o out/pdf/de-dwh-sql.pdf
 				;;
 	"all")      mkdir -p out/html
-				mkdir -p out/pdf
 				asciidoctor src/index.adoc -o out/html/index.html
+				cp src/*.png out/html/
+				cp src/*.svg out/html/
+				mkdir -p out/pdf
 				asciidoctor-pdf src/index.adoc -o out/pdf/de-dwh-sql.pdf
 				;;
 	*)          echo "Usage: $0 {clean|html|pdf|all}"
