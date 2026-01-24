207d10734938e92c00e5cbf8073c31fb9ca98304
Gary Faircloth <arkadianriver@gmail.com>
2026-01-24 14:59:19 -0600

    use asciidoctor for #11

---
+++ docs/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+case $1 in
+	"clean")    rm -rf out/html/*
+				rm -rf out/pdf/*
+				;;
+	"html")     mkdir -p out/html
+				asciidoctor src/index.adoc -o out/html/index.html
+				;;
+	"pdf")      mkdir -p out/pdf
+				asciidoctor-pdf src/index.adoc -o out/pdf/de-dwh-sql.pdf
+				;;
+	"all")      mkdir -p out/html
+				mkdir -p out/pdf
+				asciidoctor src/index.adoc -o out/html/index.html
+				asciidoctor-pdf src/index.adoc -o out/pdf/de-dwh-sql.pdf
+				;;
+	*)          echo "Usage: $0 {clean|html|pdf|all}"
+				;;
+esac
