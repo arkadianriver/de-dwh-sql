3633ec4f1c7d94ddc7555e349a20ab1b71bc3f39
Gary Faircloth <arkadianriver@gmail.com>
2026-01-24 18:39:34 -0600

    prep for #14 and text-based diagrams

--- docs/build.sh
+++ docs/build.sh
@@ -5,19 +5,19 @@ case $1 in
 				rm -rf out/pdf/*
 				;;
 	"html")     mkdir -p out/html
-				asciidoctor src/index.adoc -o out/html/index.html
+				asciidoctor -r asciidoctor-diagram -D out/html src/de-dwh-sql.adoc
 				cp src/*.png out/html/
 				cp src/*.svg out/html/
 				;;
 	"pdf")      mkdir -p out/pdf
-				asciidoctor-pdf src/index.adoc -o out/pdf/de-dwh-sql.pdf
+				asciidoctor-pdf -r asciidoctor-diagram -o out/pdf/de-dwh-sql.pdf src/de-dwh-sql.adoc
 				;;
 	"all")      mkdir -p out/html
-				asciidoctor src/index.adoc -o out/html/index.html
+				asciidoctor -r asciidoctor-diagram -D out/html src/de-dwh-sql.adoc
 				cp src/*.png out/html/
 				cp src/*.svg out/html/
 				mkdir -p out/pdf
-				asciidoctor-pdf src/index.adoc -o out/pdf/de-dwh-sql.pdf
+				asciidoctor-pdf -r asciidoctor-diagram -o out/pdf/de-dwh-sql.pdf src/de-dwh-sql.adoc
 				;;
 	*)          echo "Usage: $0 {clean|html|pdf|all}"
 				;;
