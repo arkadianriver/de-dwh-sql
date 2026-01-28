4a492f0a6d06375f3bb31e8c1b6d58a20eaf0727
Gary Faircloth <arkadianriver@gmail.com>
2026-01-28 15:55:29 -0600

    create gold model. Fixes #28

--- src/docbuild.sh
+++ src/docbuild.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+cp docsrc/index.html ../docs/
+touch ../docs/.nojekyll
+
 case $1 in
 	"clean")    rm -rf ../docs/*
 				;;
 	"html")     mkdir -p ../docs/html
-				cp docsrc/index.html ../docs/
-				touch ../docs/.nojekyll
 				asciidoctor -r asciidoctor-diagram -D ../docs/html docsrc/de-dwh-sql.adoc
 				cp -r docsrc/media ../docs/html/
 				;;
@@ -13,8 +14,6 @@ case $1 in
 				asciidoctor-pdf -r asciidoctor-diagram -o ../docs/pdf/de-dwh-sql.pdf docsrc/de-dwh-sql.adoc
 				;;
 	"all")      mkdir -p ../docs/html
-				cp docsrc/index.html ../docs/
-				touch ../docs/.nojekyll
 				asciidoctor -r asciidoctor-diagram -D ../docs/html docsrc/de-dwh-sql.adoc --trace
 				cp -r docsrc/media ../docs/html/
 				mkdir -p ../docs/pdf
